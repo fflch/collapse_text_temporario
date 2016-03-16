9a8da77631345553fb6e0c8cd05355a38416e91b
DYdave <david.suissa@davyin.com>
2016-03-16 10:02:37 +0800

    Issue #1793108 by DYdave: Filter integration improvements: Added support for strings translation, cleanup and update of hook_wysiwyg_plugin implementation.

--- wysiwyg/ckeditor/plugin.js
+++ wysiwyg/ckeditor/plugin.js
@@ -1,3 +1,8 @@
+/**
+ * @file
+ * CKEditor native plugin to provide integration with Collapse Text.
+ */
+
 CKEDITOR.plugins.add('collapse_text',
 {
   init: function(editor)
@@ -5,10 +10,13 @@ CKEDITOR.plugins.add('collapse_text',
     /* COMMAND */
     editor.addCommand('cmdCollapseTextDialog', new CKEDITOR.dialogCommand('collapseTextDialog'));
 
+    // Use the strings provided by Drupal.settings for translation support.
+    pluginStrings = Drupal.settings.collapse_text.pluginStrings;
+
     /* BUTTON */
     editor.ui.addButton('collapse_text',
     {
-      label: 'Insert collapsible text',
+      label: pluginStrings.buttonLabel,
       command: 'cmdCollapseTextDialog',
       icon: this.path + 'button.png'
     });
@@ -16,83 +24,81 @@ CKEDITOR.plugins.add('collapse_text',
     /* DIALOG */
     CKEDITOR.dialog.add('collapseTextDialog', function (editor)
     {
-      //TODO: Labels below could be replaced with linkLang.labelName.
       return {
-        title : 'Collapsible text settings',
+        title : pluginStrings.windowLabel,
         minWidth : 300,
         minHeight : 200,
         contents :
         [{
           id : 'tab1',
-          label : 'Settings',
           elements :
           [{
             type : 'text',
             id : 'title',
-            label : 'Collapsible text title',
-            validate : CKEDITOR.dialog.validate.notEmpty("Collapsible text title should be provided")
+            label : pluginStrings.titleLabel,
+            validate : CKEDITOR.dialog.validate.notEmpty(pluginStrings.titleValidateError)
           }, {
             type : 'textarea',
             id : 'content',
-            label : 'Collapsible text content',
-            validate : CKEDITOR.dialog.validate.notEmpty("Collapsible text content should be provided")
+            label : pluginStrings.contentLabel,
+            validate : CKEDITOR.dialog.validate.notEmpty(pluginStrings.contentValidateError)
           }, {
             type : 'checkbox',
             id : 'state',
-            label : 'Initially expanded (not collapsed)'
+            label : pluginStrings.stateLabel,
           },{
             type : 'text',
             id : 'classes',
-            label : 'Optional class'
+            label : pluginStrings.classesLabel,
           }]
         }],
         onOk : function()
-               {
-                 var dialog = this;
-                 var title = dialog.getValueOf('tab1', 'title');
-                 var content = dialog.getValueOf('tab1', 'content');
-                 var classes = dialog.getValueOf('tab1', 'classes');
-                 var state = !dialog.getValueOf('tab1', 'state') ? 'collapsed' : 'collapse';
+        {
+          var dialog = this;
+          var title = dialog.getValueOf('tab1', 'title');
+          var content = dialog.getValueOf('tab1', 'content');
+          var classes = dialog.getValueOf('tab1', 'classes');
+          var state = !dialog.getValueOf('tab1', 'state') ? 'collapsed' : 'collapse';
 
-                 var openTag = '[' + state + ' title="' + title + '"' + ((classes != "") ? ' class="' + classes + '"' : "") + ']';
-                 var closeTag = '[/' + state + ']';
-                 var inplaceTag = openTag + content + closeTag;
+          var openTag = '[' + state + ' title="' + title + '"' + ((classes != "") ? ' class="' + classes + '"' : "") + ']';
+          var closeTag = '[/' + state + ']';
+          var inplaceTag = openTag + content + closeTag;
 
-                 var S = editor.getSelection();
+          var S = editor.getSelection();
 
-                 if(S == null)
-                 {
-                   editor.insertHtml(inplaceTag);
-                   return;
-                 }
+          if(S == null)
+          {
+            editor.insertHtml(inplaceTag);
+            return;
+          }
 
-                 var R = S.getRanges();
-                 R = R[0];
+          var R = S.getRanges();
+          R = R[0];
 
-                 if(R == null)
-                 {
-                   editor.insertHtml(inplaceTag);
-                   return;
-                 }
+          if(R == null)
+          {
+            editor.insertHtml(inplaceTag);
+            return;
+          }
 
-                 var startPos = Math.min(R.startOffset, R.endOffset);
-                 var endPos = Math.max(R.startOffset, R.endOffset);
+          var startPos = Math.min(R.startOffset, R.endOffset);
+          var endPos = Math.max(R.startOffset, R.endOffset);
 
-                 if(startPos == endPos)
-                 {
-                   editor.insertHtml(inplaceTag);
-                   return;
-                 }
+          if(startPos == endPos)
+          {
+            editor.insertHtml(inplaceTag);
+            return;
+          }
 
-                 var container = new CKEDITOR.dom.element('p');
-                 var fragment = R.extractContents();
+          var container = new CKEDITOR.dom.element('p');
+          var fragment = R.extractContents();
 
-                 container.appendText(openTag);
-                 fragment.appendTo(container);
-                 container.appendText(closeTag);
+          container.appendText(openTag);
+          fragment.appendTo(container);
+          container.appendText(closeTag);
 
-                 editor.insertElement(container);
-               }
+          editor.insertElement(container);
+        }
       };
       // End CKEDITOR.dialog.add.
     });
